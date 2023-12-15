14e38faf66889f536d75927bc67f82b546dc0328
Jérémie Kalonji <jeremie.kalonji@gmail.com>
2023-12-15 12:43:55 +0100

    Adding data source for test

--- DataLoader.py
+++ DataLoader.py
@@ -1,8 +1,10 @@
 import os
 import openai
+import langchain
 
 openai.api_key='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'
 
 from langchain.document_loaders import PyPDFLoader
-loader = PyPDFLoader("docs/cs229_lectures/MachineLearning-Lecture01.pdf")
+loader = PyPDFLoader("Texte_de_loi.pdf")
+
 pages = loader.load()
